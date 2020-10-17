fefaccc20394103f71f0560e45f8f37afc9a7715
Jacob Lush <jacobl4560@gmail.com>
2020-10-17 14:39:05 -0700

    Refactored everything.

---
+++ src/Item.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class Item {
+    private int metascore;
+    private double userscore;
+    private List genres;
+
+    public int getMetascore() {
+        return metascore;
+    }
+    public double getUserscore() {
+        return userscore;
+    }
+}
