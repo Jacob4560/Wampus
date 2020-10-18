218d880675089fe7b480a3617ab5dde1c06cd866
Jacob Lush <jacobl4560@gmail.com>
2020-10-17 18:01:22 -0700

    Figured out how to fetch URL. This will be used to create Item objects for sorting.

--- src/runApp.java
+++ src/runApp.java
@@ -4,6 +4,7 @@ import org.jsoup.select.Elements;
 
 import javax.swing.*;
 import java.io.IOException;
+import java.util.LinkedList;
 
 public class runApp {
 
@@ -24,19 +25,36 @@ public class runApp {
 
         frame.setVisible(true);
         // Connects to Metacritic for parsing reviews.
-        String genreChoice = "crime";
-        Document d =Jsoup.connect("https://www.metacritic.com/browse/movies/genre/metascore/" + genreChoice).get();
-        Item movie = new Item();
-        getAllMoviesAndRatings(d, d.select(".clamp-summary-wrap h3").size());
+        //String genreChoice = new Scanner(System.in).next();
+        Document d = Jsoup.connect("https://www.metacritic.com/browse/movies/genre/metascore/horror").get();
+        LinkedList<Item> movies = new LinkedList<>();
+        getItemInfo(d, movies);
+        printAllItems(movies);
     }
 
-    public static void getAllMoviesAndRatings(Document d, int x){
+    public static void printAllItems(LinkedList<Item> movies){
+        for (Item movie: movies){
+            System.out.println("Title: " + movie.getTitle());
+            System.out.println("Score: " + movie.getMetaScore());
+            System.out.println("Url: " + movie.getUrl());
+            //System.out.println("User Score: " + movie.getUserscore());
+        }
+    }
+
+    // Test method which reads the top 100 of a genre from Metacritic. Adds movie items to the
+    // given LinkedList with a title and score.
+    public static void getItemInfo(Document d, LinkedList<Item> movies){
         Elements titles = d.select(".clamp-summary-wrap h3");
         Elements scores = d.select(".metascore_anchor");
-        for (int i = 0; i < x; i++){
-            System.out.println(titles.eq(i).text());
-            System.out.println(scores.eq(3*i).text());
-            System.out.println(scores.eq(3*i+2).text());
+        Elements urls = d.select("a.title");
+        for (int i = 0; i < titles.size(); i++){
+            Item movie = new Item();
+            movie.setTitle(titles.eq(i).text());
+            // Each movie has 3 score elements, so we have to use 3*i.
+            movie.setMetaScore(Integer.parseInt(scores.eq(3*i).text()));
+            //.setUserscore(Double.valueOf(scores.eq(3*i+2).text()));
+            movie.setUrl(urls.eq(i).attr("href"));
+            movies.add(movie);
         }
     }
 }
